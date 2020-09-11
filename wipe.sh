37876c255ca0e46b0638708ee536e6011087f08e
Weissnix4711 <aldrian.thom@gmail.com>
2020-09-11 13:39:44 +0100

    Update wipe script
    Remove storage and cache dirs.
    Add comments

--- wipe.sh
+++ wipe.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Down all containers
 docker-compose down
 
+# Remove and recreate app key file
 sudo rm ./panel/data/appkey.env
 touch ./panel/data/appkey.env
 
+# Remove storage and cache
+sudo rm -rf ./panel/data/storage/*
+sudo rm -rf ./panel/data/bootstrap/cache/*
+
+# Remove db
 sudo rm -rf mysql/db/*
